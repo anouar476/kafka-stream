9d215d636741512705703caaabcbaa32d07c15e6
anouar476 <anwarmoudad476@gmail.com>
2025-10-01 13:23:46 +0100

    kafka stream function implementation

--- src/main/java/com/example/kafkastream/handlers/pageEventHandler.java
+++ src/main/java/com/example/kafkastream/handlers/pageEventHandler.java
@@ -1,12 +1,15 @@
 package com.example.kafkastream.handlers;
 
 import com.example.kafkastream.events.PageEvent;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -26,5 +29,11 @@ public class pageEventHandler {
                 Math.random()>0.5?"U1":"U2",
                 new Date(),10+new Random().nextInt(1000)
         );
+    }
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> KstreamFunction(){
+        return(input)->
+                input.filter((k,v)->v.duration()>100)
+                        .map((k,v)->new KeyValue<>(v.name(),v.duration()));
     }
 }
