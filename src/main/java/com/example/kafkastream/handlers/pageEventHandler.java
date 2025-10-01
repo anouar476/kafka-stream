e40ed88af141dd4a8f2016be181101a7058fb385
anouar476 <anwarmoudad476@gmail.com>
2025-10-01 12:53:17 +0100

    KAFKA SUPPLIER

--- src/main/java/com/example/kafkastream/handlers/pageEventHandler.java
+++ src/main/java/com/example/kafkastream/handlers/pageEventHandler.java
@@ -4,7 +4,11 @@ import com.example.kafkastream.events.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 @Component
 public class pageEventHandler {
     @Bean
@@ -14,5 +18,13 @@ public class pageEventHandler {
             System.out.println(input.date());
             System.out.println(input.name());
         };
+    }
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return () -> new PageEvent(
+                Math.random()>0.5?"P1":"P2",
+                Math.random()>0.5?"U1":"U2",
+                new Date(),10+new Random().nextInt(1000)
+        );
     }
 }
