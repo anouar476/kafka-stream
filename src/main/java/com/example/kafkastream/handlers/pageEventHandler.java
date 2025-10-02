8bef91debd18919e197088559d5095c50ebb1646
anouar476 <anwarmoudad476@gmail.com>
2025-10-02 12:21:50 +0100

    final results

--- src/main/java/com/example/kafkastream/handlers/pageEventHandler.java
+++ src/main/java/com/example/kafkastream/handlers/pageEventHandler.java
@@ -1,11 +1,16 @@
 package com.example.kafkastream.handlers;
 
 import com.example.kafkastream.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -33,7 +38,14 @@ public class pageEventHandler {
     @Bean
     public Function<KStream<String, PageEvent>, KStream<String, Long>> KstreamFunction(){
         return(input)->
-                input.filter((k,v)->v.duration()>100)
-                        .map((k,v)->new KeyValue<>(v.name(),v.duration()));
+                input
+                        //.filter((k,v)->v.duration()>100)
+                        .map((k,v)->new KeyValue<>(v.name(),v.duration()))
+                        .groupByKey(Grouped.with(Serdes.String(),Serdes.Long()))
+                        .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                        .count(Materialized.as("count-store"))
+                        .toStream()
+                        .map((k,v)->new KeyValue<>(k.key(),v)
+                        );
     }
 }
